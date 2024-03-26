9c672debc8e40c0a33b46876f6668ef92450db8e
Stefani Martinez <stefimartinez1298@gmail.com>
2024-03-26 08:38:34 -0300

    primer clase

---
+++ 00clase.py
@@ -0,0 +1,25 @@
+#ejercicios : 
+# 1.1 ejercicio 
+#print('Bienevenidos al curso de programacion') 
+
+# 1.2 
+#Escribir un programa que almacene la cadena ¡Hola Mundo! en una variable y luego
+#muestre por pantalla el contenido de la variable.
+# MyVariable = ' Hola Mundo'
+# print(MyVariable)
+
+#1.3 
+#Escribir un programa que pregunte el nombre del usuario en la consola y después
+# de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!,
+# donde <nombre> es el nombre que el usuario haya introducido.
+# Nombre = input('Ingresa tu nombre')
+# Saludo = 'Hola '
+# print(Saludo + Nombre )
+
+
+#1.4 ejercicio
+#Ingresar dos números e informar la suma, multiplicación, división y resta de ellos. Si
+#el segundo número es cero la división debe mostrar “No puede realizarse la
+#operación”
+
+
---
+++ 01clase.py
@@ -0,0 +1,63 @@
+#name = 'Stefi' #variable string
+#print('Hola', name) 
+
+#IMC
+# imc = None
+# peso = 54
+# altura = 1.59
+
+# imc = peso / altura**2
+# print('Tu Indice de Masa Corporal es de' , imc)
+
+# print(type(imc))
+
+# resultado = None
+# Num1 = 50
+# Num2 = 40
+# resultado = Num1 + Num2
+# print(resultado)
+
+# Escribir un programa que almacene la cadena "Hola Mundo" en una variable y luego muestre por pantalla el contenido de la variable (no vale hacer directamente print("Hola mundo")).
+# saludar = 'Hola Mundo'
+# print(saludar)
+
+
+# Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena "¡Hola <nombre> !", donde es el nombre que el usuario haya introducido.
+#imput ingresa datos de parte del usuario
+# nombre_usuario = input('Hola Ingrese su nombre :')
+# print('Hola', nombre_usuario)
+
+
+# Hacer un programa en el que se ingresa un número entero y por pantalla informa el doble de ese número. Se pide cualquier numero al usuario y se le muestra el doble siempre.
+# num_int = int(input('Ingresa un numero entero:'))
+# resultado = num_int*2
+# print(resultado)
+
+#1.6 
+# Hacer un programa que ingresas la edad de una persona y calcula cuántos años va a tener dentro de 23 años. Mostrar el resultado por pantalla.
+# age= int(input('Ingresa tu edad'))
+# resultado = age + 23 
+# print('En 23 años vas a tener :' , resultado ,'años')
+
+
+#1.7
+# Hacer un programa en el que se ingresa precio de un producto y que muestre el precio final con el IVA (21%). Mostrar el resultado por pantalla.
+# product1 = int(input('Ingresa el precio del producto:'))
+# product1_iva = product1*1.21
+# print('El precio con iva del producto es:', product1_iva)
+
+
+#1.8
+# Hacer un programa que ingresa un precio y calcula un descuento del 9%. Mostrar por pantalla el resultado.
+# precio = float(input('Ingrese el precio del producto:'))
+# descuento = precio * 0.09
+# resultado = precio - descuento
+# print('El precio con descuento es :', resultado)
+
+#1.9
+# Hacer un programa en el que se ingresa un número entero y por pantalla informa el
+# doble de ese número.
+# doble_num = int(input('Ingresa un numero entero:'))
+# resultado = doble_num * 2 
+# print('El doble del numero es:' , resultado)
+
