3800c70d7aacd8dce268dc5c388dc3435e3fbdab
Stefani Martinez <stefimartinez1298@gmail.com>
2024-04-07 01:15:59 -0300

    clases 2 y 3 + ejercicios resueltos

---
+++ 02clase.py
@@ -0,0 +1,42 @@
+# number = int(input('Ingresa un número por favor:'))
+# resultado = number < 10 or number > 100
+# print(resultado)  
+
+
+#CONVERSOR DE TEMPERATURA
+# Crea un programa que solicite al usuario ingresar una temperatura en grados Celsius y
+# luego convierta esta temperatura a grados Fahrenheit. Para ello, utiliza la siguiente fórmula
+# de conversión: Fahrenheit = Celsius * 9/5 + 32.
+# celsius = float(input('Ingresa la temperatura en grados Celsius: '))
+# fahrenhei = celsius * 9/5 + 32
+# print(f'La temperatura en Fahrenheit es de {fahrenhei}')
+
+
+#Calculadora de área de un rectángulo
+# Diseña un programa que pida al usuario ingresar la longitud y el ancho de un rectángulo.
+# Luego, calcula el área del rectángulo utilizando la fórmula: Área = longitud * ancho. Muestra
+# el resultado del cálculo
+# longitud = float(input('Porfavor ingrese la longitud del rectángulo:'))
+# ancho = float(input('Porfavor ingrese el ancho del rectángulo:'))
+# area = longitud * ancho 
+# print(f'El area del rectagulo es {area}')
+
+
+# Conversor de monedas (Dólares a Pesos Argentinos):
+# Crea un programa que solicite al usuario ingresar una cantidad en dólares y conviértela a
+# pesos argentinos. Utiliza un tipo de cambio fijo de 1050 pesos argentinos por dólar. Muestra
+# el resultado de la conversión.
+# dolar = float(input('Ingrese el monto a convertir en dolares:'))
+# pesos = dolar * 1050
+# print(f'La cantidad en pesos argentinos es {pesos}')
+
+
+# Calculadora de velocidad:
+# Desarrolla un programa que requiera al usuario ingresar la distancia recorrida en kilómetros
+# y el tiempo tomado en horas. Luego, calcula y muestra la velocidad promedio en kilómetros
+# por hora. Emplea la fórmula: Velocidad = Distancia / Tiempo. 
+
+# distancia = float(input('Ingrese la distancia recorrida en km'))
+# tiempo = float(input('Ingrese las horas que le tomo recorrer esa distancia'))
+# velocidad_prom = distancia / tiempo 
+# print(f'La velocidad promedio es {velocidad_prom} km por hora')
---
+++ 03clase.py
@@ -0,0 +1,57 @@
+#Estructuras de decisión simple 
+# number = int(input('Ingresa un número'))
+# if number >= 0 : 
+#     print(f'El {number} es un número positivo')
+# else : 
+#     print(f'El {number} es un número negativo') 
+
+#Actividad 1 
+# operacion = ((3+2)/(2*5)) **2
+# print(f'El resultado de la operación aritmética es {operacion}')
+
+#Actividad 2 
+#horas de trabajo 
+# horas_trabajadas = int(input('Ingresa las horas que trabaja'))
+# hora_costo = float(input('Ingresa el pago por hora'))
+# pago = horas_trabajadas * hora_costo 
+# print(f'El pago correspondiente es {pago}')
+
+#Actividad 3
+# payasos = int(input('Ingresa la cantidad de payasos vendidos:'))
+# muñecas = int(input('Ingresa la cantidad de muñecas vendidas:'))
+# payaso_peso = 112
+# muñeca_peso = 75 
+# peso_paquete = (payasos * payaso_peso) + (muñecas * muñeca_peso)
+# print(f'El peso de paquete completo seria de {peso_paquete} gramos')
+
+#Actividad 4 
+# number = int(input('Ingresa un número'))
+# if number >= 0 and number <= 100 : 
+#     print(f'El {number} es un número positivo y se encuentra entre el rango del 1 al 100')
+# elif number >= 0 :
+#     print('El número es positivo')
+# else : 
+#     print(f'El {number} es un número negativo') 
+
+#Actividad 5
+# nombre_empleado = input('Ingresa el nombre del empleado/a')
+# precio_horas = 9576
+# horas_trabajadas = float(input(f'Ingresa las horas trabajadas de {nombre_empleado}'))
+# sueldo_bruto = horas_trabajadas * precio_horas
+# jubilacion = (sueldo_bruto * 11.5) / 100
+# obra_social = (sueldo_bruto * 4.5) / 100
+# imp_ganancias = (sueldo_bruto * 6 ) / 100 
+# imp_ganancias2 = (sueldo_bruto * 10 ) / 100 
+# sueldo_neto = sueldo_bruto - jubilacion - obra_social 
+# sueldo_neto1 = sueldo_neto - imp_ganancias
+# sueldo_neto2 = sueldo_neto - imp_ganancias2 
+
+# if sueldo_bruto < 700000 :
+#     print(f'Los datos de {nombre_empleado} son : sueldo bruto: ${sueldo_bruto} - Jubilación : ${jubilacion} - Obra Social : ${obra_social} - Sueldo Neto a cobrar : $ {sueldo_neto}')
+# elif sueldo_bruto < 800000 :
+#     print(f'Los datos de {nombre_empleado} son : -Sueldo Bruto: ${sueldo_bruto} - Jubilación : ${jubilacion} - Obra Social : ${obra_social} - Impuestos a la ganancia : $ {imp_ganancias} -Sueldo Neto a cobrar : $ {sueldo_neto1}')
+# else:
+#     print(f'Los datos de {nombre_empleado} son : -Sueldo Bruto: ${sueldo_bruto} - Jubilación : ${jubilacion} - Obra Social : ${obra_social} - Impuestos a la ganancia : $ {imp_ganancias2} -Sueldo Neto a cobrar : $ {sueldo_neto2}')
+
+    
+
